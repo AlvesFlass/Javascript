a7fbbdc5b169e95f31741d975e1956e0c5e003aa
Alvaro <alvaro23oliveira@gmail.com>
2025-03-01 10:05:03 -0300

    ATT

--- Test.js
+++ Test.js
@@ -1,4 +1,4 @@
-//onsole.log(user)
+/*(nsole.log(user)
 // var user = "Alvaro"
 
 
@@ -9,7 +9,7 @@ var email = "joao@email.com"
 {
 
 
-    //Escopo de global
+    /*Escopo de global
     console.log(email)
 }
 
@@ -36,11 +36,11 @@ Erro
     address = "Rua x"
     console.log(address)
  }
+// let usarname = "rodrigo"
 
- let usarname = "rodrigo"
- let userName = "JOA"
+//et userName = "JOA"
 
- console.log(usarname)
+/*onsole.log(usarname)
  console.log(userName)
 
  // Podem os iniciar o começo das variaveis com carater  especial 
@@ -67,15 +67,70 @@ console.log("?")
 // EScreva a primeira palavra tudo minusculo , quando estiver nome de variavel com nome composto 
 // Utilize camelCase união com o camelCase  exp: camel Case <-
 
+//let productManage="Teclado"
+//let firstName = "alvaro"
+//let lastName = "rodrigo"
 
-let productManage="Teclado"
-let firstName = "alvaro"
-let lastName = "rodrigo"
 
 //RECOMENDAÇÃO snake_case 
 
-let product_name = "teclado"
-let first_name = "rodrigo "
-let last_name = "gongon"
+//let product_name = "teclado"
+//let first_name = "rodrigo "
+//let last_name = "gongon
 
 
+
+/**
+ * Javascript e uma linguagem dinamica com tipos dinamicos. isso signiifica que o tipo
+ * da variavel e defiinida dinamicamente pelo tipo do valor atribuido
+ * não e necessario declarar o tipo
+ * 
+ * alem disso, e possivel reatribuir uma mesma variavel com um tipo diferente 
+ * 
+ * tipos Primitivos 
+ * 
+ * string se refere a texto
+ * number numero 
+ * boolean   verdadeiro ou falso true or false
+ * null nulo tipo de variavel que o valor e vazio
+ * undefined tipo de dado que o proprio js atribui automaticamente a uma variavel
+ * que nao tem ou nao armazena nenhum valor 
+ * 
+ * object 
+ * array
+*/
+
+
+/*string typeof=verifica  o conteudo da variavel 
+let usarname = "rodrigo "
+console.log (usarname)
+console.log(typeof username)
+
+console.log ('Uma string com aspas')
+console.log('Uma string com apostrofo (aspas simples)')
+
+
+console.log ("Uma string com'aspas simples'dentro de dulpa ")
+
+
+console.log (" Uma sring com acento grave permite  escrever multiplas linhas " )
+ 
+*/
+
+// Template literals (template strings , intterpolação de string )
+let usarname = "rodrigo "
+let email = "rodrigo@gamil.com "
+
+// passar mais de um parametro
+
+console.log (usarname, email ,'  teste')
+
+//concatenar texto sinal de + 
+let message = "Ola, " + usarname + ". Voce conectou com o e-mail" + email
+
+console.log (message)
+
+//template literals 
+
+console.log (`a, ${username}. Voce conectou com o e-mail
+    ${email }. `)
