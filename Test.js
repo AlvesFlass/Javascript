4fbe62ab7d687d5bdb4b3ca7380079fad79f59e1
Alvaro <alvaro23oliveira@gmail.com>
2025-03-01 09:36:17 -0300

    Atualização js

--- Test.js
+++ Test.js
@@ -1,1 +1,81 @@
-console.log("Hello word")
+//onsole.log(user)
+// var user = "Alvaro"
+
+
+var user = "Alvaro"
+
+var email = "joao@email.com"
+
+{
+
+
+    //Escopo de global
+    console.log(email)
+}
+
+{
+ var age =  18
+}
+
+console.log(age)
+
+
+
+{
+Erro 
+
+// let vai garantir que agente utilize a variavel onde ela foi criada e dsentro do escopo onde ela existe 
+  //console.log(adress)
+ //let address = "Ruax"
+ }
+
+
+ let address = "Rua x"
+
+ {
+    address = "Rua x"
+    console.log(address)
+ }
+
+ let usarname = "rodrigo"
+ let userName = "JOA"
+
+ console.log(usarname)
+ console.log(userName)
+
+ // Podem os iniciar o começo das variaveis com carater  especial 
+
+ let $email ="rodrigo@gmail.com"
+ let _email = "joa@gmail.com"
+
+ let Ação = "Cadastrar"
+
+ console.log($email)
+ console.log(_email)
+ console.log(Ação)
+
+ // NÃO ´PODEMOS 
+ // UTILIZAR NO COMEÇO DA VARIAVEL NUMEROS EXP
+ // LET 1USER = "ANNA"
+
+//  RECOMENDAÇÕES sempre escreva em ingles
+// NÃO USE CARACTERES COM ACENTO ETC
+let productName = "?"
+console.log("?")
+
+//RECOMENDAÇÃO camelCase 
+// EScreva a primeira palavra tudo minusculo , quando estiver nome de variavel com nome composto 
+// Utilize camelCase união com o camelCase  exp: camel Case <-
+
+
+let productManage="Teclado"
+let firstName = "alvaro"
+let lastName = "rodrigo"
+
+//RECOMENDAÇÃO snake_case 
+
+let product_name = "teclado"
+let first_name = "rodrigo "
+let last_name = "gongon"
+
+
