c2da3a471bff853724f2267ab97eece1c4a4290f
Alvaro <alvaro23oliveira@gmail.com>
2025-03-07 19:26:44 -0300

    ATT

--- script.js
+++ script.js
@@ -1,153 +1,38 @@
-/*(nsole.log(user)
-// var user = "Alvaro"
+//console.log(true)
+//console.log(false)
 
 
-var user = "Alvaro"
+//let isLoading = true
+//console.log(typeof isLoading)
 
-var email = "joao@email.com" 
+//boolean armazena valores verdadeiro e falsos 
 
-{
-
-
-    /*Escopo de global
-    console.log(email)
-}
-
-{
- var age =  18
-}
-
-console.log(age)
-
-
-
-{
-Erro 
-
-// let vai garantir que agente utilize a variavel onde ela foi criada e dsentro do escopo onde ela existe 
-  //console.log(adress)
- //let address = "Ruax"
- }
-
-
- let address = "Rua x"
-
- {
-    address = "Rua x"
-    console.log(address)
- }
-// let usarname = "rodrigo"
-
-//et userName = "JOA"
-
-/*onsole.log(usarname)
- console.log(userName)
-
- // Podem os iniciar o começo das variaveis com carater  especial 
-
- let $email ="rodrigo@gmail.com"
- let _email = "joa@gmail.com"
-
- let Ação = "Cadastrar"
-
- console.log($email)
- console.log(_email)
- console.log(Ação)
-
- // NÃO ´PODEMOS 
- // UTILIZAR NO COMEÇO DA VARIAVEL NUMEROS EXP
- // LET 1USER = "ANNA"
-
-//  RECOMENDAÇÕES sempre escreva em ingles
-// NÃO USE CARACTERES COM ACENTO ETC
-let productName = "?"
-console.log("?")
-
-//RECOMENDAÇÃO camelCase 
-// EScreva a primeira palavra tudo minusculo , quando estiver nome de variavel com nome composto 
-// Utilize camelCase união com o camelCase  exp: camel Case <-
-
-//let productManage="Teclado"
-//let firstName = "alvaro"
-//let lastName = "rodrigo"
-
-
-//RECOMENDAÇÃO snake_case 
-
-//let product_name = "teclado"
-//let first_name = "rodrigo "
-//let last_name = "gongon
-
-
-
-/**
- * Javascript e uma linguagem dinamica com tipos dinamicos. isso signiifica que o tipo
- * da variavel e defiinida dinamicamente pelo tipo do valor atribuido
- * não e necessario declarar o tipo
- * 
- * alem disso, e possivel reatribuir uma mesma variavel com um tipo diferente 
- * 
- * tipos Primitivos 
- * 
- * string se refere a texto
- * number numero 
- * boolean   verdadeiro ou falso true or false
- * null nulo tipo de variavel que o valor e vazio
- * undefined tipo de dado que o proprio js atribui automaticamente a uma variavel
- * que nao tem ou nao armazena nenhum valor 
- * 
- * object 
- * array
-*/
-
-
-/*string typeof=verifica  o conteudo da variavel 
-let usarname = "rodrigo "
-console.log (usarname)
-console.log(typeof username)
-
-console.log ('Uma string com aspas')
-console.log('Uma string com apostrofo (aspas simples)')
-
-
-console.log ("Uma string com'aspas simples'dentro de dulpa ")
-
-
-console.log (" Uma sring com acento grave permite  escrever multiplas linhas " )
+//Undefined (Indefinido ) e null  
  
-*/
+//let emptiness
+//console.log("0 valor e :", emptiness)
 
-/* Template literals (template strings , intterpolação de string )
-let usarname = "rodrigo "
-let email = "rodrigo@gamil.com "
+//let empty = null
+//console.log("0 valor e :", empty)
 
-// passar mais de um parametro
-
-console.log (usarname, email ,'  teste')
-
-//concatenar texto sinal de + 
-let message = "Ola, " + usarname + ". Voce conectou com o e-mail" + email
-
-console.log (message)
-
-//template literals 
-
-console.log (`a, ${username}. Voce conectou com o e-mail
-    ${email }. `)   */
+//comversão de tipos 
 
 
-//number
-    console.log(typeof 5)
+let value ="9"
+console.log(typeof value  ) 
+console.log(typeof Number (value))
 
-//Inteiro positivo
-    console.log(5)
-//Inteiro negativo
-    console.log(- 5)
-//Numero reaus ou float
-    console.log(7.5)
+let age = 18 
+console.log (typeof age. toString())
+console.log(typeof String (age))
+ 
+let option = -20
+console.log(Boolean(option))
+console.log(typeof Boolean (Option))
 
-    console.log(12.5 / 10 )
 
+
+ 
 
 
 
